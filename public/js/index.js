9f4164b24af333f99ea498e64261fcc520be5d91
Xavier Trinidad <xmtrinidad@gmail.com>
2018-05-06 17:02:57 -0700

    Put script back in head section so that materialize toasts would show

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,24 @@
-$('.sidenav').sidenav();
-$('.modal').modal();
-$('.tabs').tabs();
-$('select').formSelect();
-$('.dropdown-trigger').dropdown();
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+  $('.modal').modal();
+});
+
+$(document).ready(function () {
+  $('.tabs').tabs();
+});
+
+$(document).ready(function () {
+  $('select').formSelect();
+});
+
+$(document).ready(function () {
+  $('.dropdown-trigger').dropdown();
+});
+
+
+
+
 
